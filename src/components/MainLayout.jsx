93decafcfd209bb8de71de7837e1350243673ace
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-21 05:28:09 +0530

    we write the code for direct login the user

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import LeftSidebar from './LeftSidebar'
+import { loginUser } from '@/api/api'
+import { useDispatch } from 'react-redux'
+import { setUsers } from '@/redux/slice/auth.slice'
 
 const MainLayout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const {message , success , data} = await loginUser({method : "POST" , url : "http://localhost:4000/api/v1/auth/direct-login", contentType : "application/json"});
+
+      if(success){
+        dispatch(setUsers(data));
+
+      }else{
+        console.log("We get error during direct login the user !",message)
+      }
+    })
+  },[])
   return (
     <div >
          <LeftSidebar/>
